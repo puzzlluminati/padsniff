c1b5e3a1ac98b5e8bc89dbf75468852e0a24c383
Will Medlar <will.medlar@rewardstyle.com>
2016-11-29 20:29:31 -0600

    Simplify package dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='Padsniff',
+    name='padsniff',
     version='0.1.0',
     description='Command-line tool to sniff Puzzle & Dragons data.',
     author='Will Medlar',
     author_email='wmmedlar@gmail.com',
+    install_requires=[
+        'click==6.6',
+        'mitmproxy==0.18.2',
+    ],
     packages=['padsniff'],
     entry_points={
         'console_scripts': [
