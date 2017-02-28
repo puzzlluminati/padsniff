4a44ce44063281e9f0ebe2c39e8e80316028865e
Will Medlar <will.medlar@rewardstyle.com>
2017-02-27 19:59:39 -0600

    Add additional test documentation

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -8,7 +8,10 @@ from pytest import fixture
 
 @fixture(autouse=True)
 def join_threads():
-    # can't join main thread because it's running our tests
+    """
+    Join all threads except the main thread to ensure none
+    are running between tests.
+    """
     threads = set(all_threads()) - {main_thread()}
     for thread in threads:
         thread.join()
@@ -18,6 +21,7 @@ class TestHandlerThread:
 
 
     def test_instance_tracking(self):
+        """Test that HandlerThread keeps track of instances of itself."""
         assert len(HandlerThread.instances) == 0
 
         handler_thread1 = HandlerThread()
@@ -29,12 +33,16 @@ class TestHandlerThread:
         assert handler_thread2 in HandlerThread.instances
         assert regular_thread not in HandlerThread.instances
 
+        # HandlerThread.instances is a WeakSet, so allowing the gc to
+        # collect handler_thread2 will remove it from the set
         del handler_thread2
 
         assert len(HandlerThread.instances) == 1
 
 
 def test_parallelize_nonblocking():
+    """Test basic functionality of parallelize decorator."""
+    # only main thread should be running
     assert len(all_threads()) == 1
     event = Event()
 
@@ -46,13 +54,14 @@ def test_parallelize_nonblocking():
     assert len(all_threads()) == 2
 
     event.set()
-    # give the thread time to wake up
+    # give the thread time to wake up and exit
     sleep(0.1)
 
     assert len(all_threads()) == 1
 
 
 def test_parallellize_with_args(mocker):
+    """Test that parallelize decorator passes arguments to the target function."""
     mock = mocker.Mock()
     name = mock.__qualname__ = 'mock'
     args = 'can dank memes', 'melt steel beams?'
@@ -67,4 +76,4 @@ def test_parallellize_with_args(mocker):
         args=args,
         kwargs=kwargs,
     )
-    assert mock.start.called_once_with()
+    assert mock.start.called_once()
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -17,26 +17,40 @@ class TestProxy:
         host, port = '1.2.3.4', 12345
         proxy = Proxy(host=host, port=port)
 
+        # test that parent class initializes with proper arguments
         assert proxy.options.listen_host == host
         assert proxy.options.listen_port == port
         assert proxy.options.mode == 'transparent'
+
+        # test that the handlers are equivalent, but not the same reference
         assert proxy.handlers == Proxy.handlers
+        assert proxy.handlers is not Proxy.handlers
 
 
-    def test_on_decorator(self):
+    def test_inst_on_decorator(self, mocker):
+        """Test basic functionality of the class-level on decorator."""
         endpoint = 'endpoint'
         proxy = Proxy()
 
-        assert not proxy.handlers[endpoint]
-
+        # test that instance decorator adds only to instance variable
         @proxy.on(endpoint, blocking=True)
         def func():
             pass
 
         assert func in proxy.handlers[endpoint]
         assert func not in Proxy.handlers[endpoint]
-        assert proxy.on('endpoint2', blocking=True)(func) is func
-        assert proxy.on('endpoint2', blocking=False)(func) is func
+
+        # test that padsniff.parallel.parallelize is only called for blocking=False
+        with mocker.patch('padsniff.parallel.parallelize') as mock:
+            proxy.on(endpoint, blocking=True)(func)
+            assert mock.not_called()
+
+            proxy.on(endpoint, blocking=False)(func)
+            assert mock.called_once_with(func)
+
+        # test that decorated function is returned invariant
+        assert proxy.on(endpoint, blocking=True)(func) is func
+        assert proxy.on(endpoint, blocking=False)(func) is func
 
 
     def test_response_filtering(self, mocker, flow):
@@ -85,23 +99,38 @@ class TestProxy:
         assert func2.not_called()
 
 
-def test_blocking_on_decorator(mocker):
+def test_on_decorator(mocker):
+    """Test basic functionality of the module-level on decorator."""
     endpoint = 'endpoint'
     proxy = Proxy()
 
+    # test that module-level decorator adds only to class variable
     @on(endpoint, blocking=True)
-    def func1():
+    def func():
         pass
 
-    assert func1 in Proxy.handlers[endpoint]
-    assert func1 not in proxy.handlers[endpoint]
+    assert func in Proxy.handlers[endpoint]
+    assert func not in proxy.handlers[endpoint]
 
-    with mocker.patch('padsniff.proxy.Proxy.on') as mock:
-        @on(endpoint, blocking=True)
-        def func2():
-            pass
+    # test that padsniff.parallel.parallelize is only called for blocking=False
+    with mocker.patch('padsniff.parallel.parallelize') as mock:
+        on(endpoint, blocking=True)(func)
+        assert mock.not_called(func)
 
-        assert mock.called_once_with(Proxy, func2, blocking=True)
+        on(endpoint, blocking=False)(func)
+        assert mock.called_once_with(func)
+
+    # test that decorated function is returned invariant
+    assert on(endpoint, blocking=True)(func) is func
+    assert on(endpoint, blocking=False)(func) is func
+
+    # test that an arbitrary class can be specified
+    class NewProxy:
+        on = mocker.Mock()
+
+    on(endpoint, blocking=True, cls=NewProxy)
+
+    assert NewProxy.on.called_once_with(endpoint, blocking=True)
 
 
 def test_is_gungho(flow):
